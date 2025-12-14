1ec567a999f9d5d74d319b154286bf1872d1ecac
BrownieCake- <91715384+BlueMicro233@users.noreply.github.com>
2025-12-15 00:42:08 +0800

    Delete Project1/tex.cpp

--- Project1/tex.cpp
+++
@@ -1,9 +0,0 @@
-#include <glad/glad.h>
-#include <iostream>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-static int height, width, nrChannels;
-unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
-
-unsigned int texture;
