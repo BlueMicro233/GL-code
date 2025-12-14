89d707a1ba8bff5f867b3c230072afec82a35b86
BrownieCake- <iwiwiwiwiwi@Outlook.com>
2025-12-14 18:13:01 +0800

    improve renderer, make it possible to render blackhole(shader from shadertoy)

---
+++ Project1/tex.cpp
@@ -0,0 +1,9 @@
+#include <glad/glad.h>
+#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int height, width, nrChannels;
+unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+
+unsigned int texture;
